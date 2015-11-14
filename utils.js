b2a954e703aadb6b881d1a4130d54b1e33ee051e
Jenny Veens <jennyveens@gmail.com>
2015-11-14 14:51:58 -0500

    Completed exercise 5, added touch capture script to utils.js

--- utils.js
+++ utils.js
@@ -1,35 +1,64 @@
 if (!window.requestAnimationFrame) {
-  window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
-                                  window.mozRequestAnimationFrame ||
-                                  window.oRequestAnimationFrame ||
-                                  window.msRequestAnimationFrame ||
-                                  function (callback) {
-                                  return window.setTimeout(callback, 1000/60);
-                                  });
+		window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
+																		window.mozRequestAnimationFrame ||
+																		window.oRequestAnimationFrame ||
+																		window.msRequestAnimationFrame ||
+																		function (callback) {
+																			return window.setTimeout(callback, 1000/60);
+																		});
 }
 
 var utils = {};
 
 utils.captureMouse = function (element) {
-  var mouse = {x: 0, y: 0};
+		var mouse = {x: 0, y: 0};
 
-  element.addEventListener('mousemove', function (event) {
-    var x, y;
-    if (event.pageX || event.pageY) {
-      x = event.pageX;
-      y = event.pageY;
-    } else {
-      x = event.clientX + document.body.scrollLeft +
-          document.documentElement.scrollLeft;
-      y = event.clientY + document.body.scrollTop +
-          document.documentElement.scrollTop;
-    }
-    x -= element.offsetLeft;
-    y -= element.offsetTop;
+		element.addEventListener('mousemove', function (event) {
+				var x, y;
+				if (event.pageX || event.pageY) {
+						x = event.pageX;
+						y = event.pageY;
+				} else {
+						x = event.clientX + document.body.scrollLeft +
+								document.documentElement.scrollLeft;
+						y = event.clientY + document.body.scrollTop +
+								document.documentElement.scrollTop;
+				}
+				x -= element.offsetLeft;
+				y -= element.offsetTop;
 
-    mouse.x = x;
-    mouse.y = y;
-  }, false);
+				mouse.x = x;
+				mouse.y = y;
+		}, false);
 
-  return mouse;
+		return mouse;
 };
+
+utils.captureTouch = function (element) {
+					var touch = {x: null, y: null, isPressed: false};
+					element.addEventListener('touchstart', function (event) {
+							touch.isPressed = true;
+}, false);
+					element.addEventListener('touchend', function (event) {
+							touch.isPressed = false;
+							touch.x = null;
+							touch.y = null;
+}, false);
+					element.addEventListener('touchmove', function (event) {
+							var x, y, touch_event = event.touches[0]; //first touch
+							if (touch_event.pageX || touch_event.pageY) {
+									x = touch_event.pageX;
+									y = touch_event.pageY;
+							} else {
+									x = touch_event.clientX + document.body.scrollLeft +
+												document.documentElement.scrollLeft;
+									y = touch_event.clientY + document.body.scrollTop +
+												document.documentElement.scrollTop;
+							}
+							x -= offsetLeft;
+							y -= offsetTop;
+							touch.x = x;
+							touch.y = y;
+					}, false);
+					return touch;
+			};
