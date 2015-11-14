2b9af779cf09a65db3bd24a19ab53195ffe90f6c
Jenny Veens <jennyveens@gmail.com>
2015-11-14 17:31:21 -0500

    Adding ball script and 03-02 bobbing exercise. Not quite working yet - need to add parseColor function.

--- ball.js
+++ ball.js
@@ -10,7 +10,7 @@ function Ball (radius, color) {
 	this.rotation = 0;
 	this.scaleX = 1;
 	this.scaleY = 1;
-	this.color = utils.parseColor(color);
+	// this.color = utils.parseColor(color);
 	this.lineWidth = 1;
 }
 
