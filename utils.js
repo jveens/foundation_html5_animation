761b54f6204a18fe7cfdd3a3bee0fb269f9f59b0
Jenny Veens <jennyveens@gmail.com>
2015-11-14 19:28:33 -0500

    Added parseColor to utils.js

--- utils.js
+++ utils.js
@@ -32,33 +32,52 @@ utils.captureMouse = function (element) {
 		}, false);
 
 		return mouse;
-};
+}; // end utils.captureMouse
 
 utils.captureTouch = function (element) {
-					var touch = {x: null, y: null, isPressed: false};
-					element.addEventListener('touchstart', function (event) {
-							touch.isPressed = true;
-}, false);
-					element.addEventListener('touchend', function (event) {
-							touch.isPressed = false;
-							touch.x = null;
-							touch.y = null;
-}, false);
-					element.addEventListener('touchmove', function (event) {
-							var x, y, touch_event = event.touches[0]; //first touch
-							if (touch_event.pageX || touch_event.pageY) {
-									x = touch_event.pageX;
-									y = touch_event.pageY;
-							} else {
-									x = touch_event.clientX + document.body.scrollLeft +
-												document.documentElement.scrollLeft;
-									y = touch_event.clientY + document.body.scrollTop +
-												document.documentElement.scrollTop;
-							}
-							x -= offsetLeft;
-							y -= offsetTop;
-							touch.x = x;
-							touch.y = y;
-					}, false);
-					return touch;
-			};
+		var touch = {x: null, y: null, isPressed: false};
+		element.addEventListener('touchstart', function (event) {
+				touch.isPressed = true;
+		}, false);
+		element.addEventListener('touchend', function (event) {
+				touch.isPressed = false;
+				touch.x = null;
+				touch.y = null;
+		}, false);
+		element.addEventListener('touchmove', function (event) {
+				var x, y, touch_event = event.touches[0]; //first touch
+				if (touch_event.pageX || touch_event.pageY) {
+						x = touch_event.pageX;
+						y = touch_event.pageY;
+				} else {
+						x = touch_event.clientX + document.body.scrollLeft +
+									document.documentElement.scrollLeft;
+						y = touch_event.clientY + document.body.scrollTop +
+									document.documentElement.scrollTop;
+				}
+				x -= offsetLeft;
+				y -= offsetTop;
+				touch.x = x;
+				touch.y = y;
+		}, false);
+		return touch;
+}; // end utils.captureTouch
+
+utils.parseColor = function (color, toNumber) {
+	if (toNumber === true) {
+		if (typeof color === 'number') {
+			return (color | 0); // chop off decimal
+		}
+		if (typeof color === 'string' && color[0] === '#') {
+			color = color.slice[1];
+		}
+		return window.parseInt(color, 16);
+	}
+	else {
+		if (typeof color === 'number') {
+			// make sure our hexidecimal number is padded out
+			color = '#' + ('00000' + (color | 0).toString(16)).substr(-6);
+		}
+		return color;
+	}
+}; // end utils.parseColor
