7743755a5a6f3e6ec0de920764f411ba75170968
Jenny <jennyveens@gmail.com>
2015-11-17 16:41:20 -0500

    Added assets folder for Ch.4 and updated paths

--- utils.js
+++ utils.js
@@ -1,11 +1,11 @@
 if (!window.requestAnimationFrame) {
 		window.requestAnimationFrame = (window.webkitRequestAnimationFrame ||
-																		window.mozRequestAnimationFrame ||
-																		window.oRequestAnimationFrame ||
-																		window.msRequestAnimationFrame ||
-																		function (callback) {
-																			return window.setTimeout(callback, 1000/60);
-																		});
+										window.mozRequestAnimationFrame ||
+										window.oRequestAnimationFrame ||
+										window.msRequestAnimationFrame ||
+										function (callback) {
+											return window.setTimeout(callback, 1000/60);
+										});
 }
 
 var utils = {};
